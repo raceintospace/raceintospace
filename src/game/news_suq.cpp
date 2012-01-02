82da7ad4e0a33db7e89d6dcf0a8fe38785c2304c
Michael K McCarty <mkmccarty@gmail.com>
2012-01-01 22:58:07 -0800

    Added a comment that the BudgetMods table now only uses the easy level settings.

--- src/game/news_suq.cpp
+++ src/game/news_suq.cpp
@@ -144,10 +144,11 @@ char REvent(char plr)
         }
 
         fin = sOpen("NTABLE.DAT", "rb", 0);
+        // This is ignoring the budget mods based on the selected game level.
         fseek(fin, (sizeof(int16_t)) * ((plr * 60) + (j * 10) + random(10)) , SEEK_SET);
         fread(&m, sizeof m, 1, fin);
-        Swap16bit(m);
-//    m=BudgetMods[Data->P[plr].Level][(j*10)+random(10)];  // orig code
+        Swap16bit(m); 
+        //    m=BudgetMods[Data->P[plr].Level][(j*10)+random(10)];  // orig code
         fclose(fin);
 
         Data->P[plr].Budget += m;
