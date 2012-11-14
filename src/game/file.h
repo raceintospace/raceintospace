7e31a646434370f1c6d0c627ad2be62f97f8d304
Will Glynn <will@willglynn.com>
2012-11-13 22:16:59 -0600

    Add a basic filesystem abstraction

---
+++ src/game/file.h
@@ -0,0 +1,28 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <stdint.h>
+
+class File
+{
+public:
+    File(void * handle);
+    ~File();
+    
+    void close();
+    
+    int64_t read(void * buffer, uint64_t length);
+    
+    int64_t write(const void * buffer, uint64_t length);
+    void flush();
+    
+    void seek(uint64_t position);
+    uint64_t tell();
+    uint64_t length();
+    bool eof();
+    
+protected:
+    void * m_handle;
+};
+
+#endif // FILE_H
