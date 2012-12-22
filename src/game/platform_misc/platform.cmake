95e2c305f9cc58244eb8073aa0f3aec020166c07
joequant <joequant@gmail.com>
2012-12-22 12:50:42 +0800

    build executable for misc platforms

---
+++ src/game/platform_misc/platform.cmake
@@ -0,0 +1,21 @@
+# Here too. This depends on probing the libsdl that may not have existed
+# until after we started the build process.
+add_definitions(-DHAVE_SDL_GETENV)
+
+# Make sure we can fine fake_unistd.h
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/platform_misc)
+
+set(app "raceintospace")
+
+add_executable(${app}
+  ${game_sources}
+  music_vorbis.cpp
+  platform_misc/main.c
+  )
+
+target_link_libraries(${app}
+  ${game_libraries}
+  raceintospace_display ${raceintospace_display_libraries}
+  )
+
+add_dependencies(${app} libs)
