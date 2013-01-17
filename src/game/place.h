a072664fa786dfe9e2ea2be0fd5d41de55031618
Will Glynn <will@willglynn.com>
2013-01-16 22:50:28 -0600

    Remove rdfull.but
    This results in a significant reduction of BigHardMe(), but it still
    also looks at LIFTOFF.ABZ, which hasn't been PNGified yet. Sigh.
    
    Incidentally, this also closes #107.

--- src/game/place.h
+++ src/game/place.h
@@ -4,7 +4,7 @@
 int Help(const char *FName);
 void Draw_Mis_Stats(char plr, char index, int *where, char mode);
 void PatchMe(char plr, int x, int y, char prog, char poff, unsigned char coff);
-void BigHardMe(char plr, int x, int y, char hw, char unit, char sh, unsigned char coff);
+void BigHardMe(char plr, int x, int y, char hw, char unit, char sh);
 void AstFaces(char plr, int x, int y, char face);
 void SmHardMe(char plr, int x, int y, char prog, char planet, unsigned char coff);
 int BChoice(char plr, char qty, char *Name, char *Imx);
