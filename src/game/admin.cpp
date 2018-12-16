raceintospace/raceintospace

commit eb75c92c3ca0a775f6aed332d270ba31dc623393

Remove EVENT.TMP save game file
Now using memory buffers to keep track of saved Event cards.

src/game/admin.cpp
@@ -520,9 +520,16 @@ void FileAccess(char mode)
                         // File Structure is 84 longs 42 per side
                     }
 
-                    fout = sOpen("EVENT.TMP", "wb", 1);
-                    fwrite(load_buffer, eventSize, 1, fout);
-                    fclose(fout);
+                    // Save Event information
+                    if (interimData.eventBuffer) {
+                        free(interimData.eventBuffer);
+                    }
+
+                    interimData.eventBuffer = (char *) malloc(eventSize);
+                    interimData.eventSize = eventSize;
+                    memcpy(interimData.eventBuffer, load_buffer, eventSize);
+                    interimData.tempEvents = (OLDNEWS *) interimData.eventBuffer;
+
                     free(load_buffer);
 
                     if (!(SaveHdr->Country[0] == 6 || SaveHdr->Country[1] == 7 || SaveHdr->Country[0] == 8 || SaveHdr->Country[1] == 9)) {
@@ -765,15 +772,13 @@ void FileAccess(char mode)
                 // Save Replay information
                 fwrite(interimData.tempReplay, interimData.replaySize, 1, fin);
 
-                fout = sOpen("EVENT.TMP", "rb", 1); // Save Event File
-                left = 32000; // copy EVENT.TMP FILE
+                // Save Event information
+                // todo: how many bytes do we read?
+                interimData.eventBuffer = (char *) malloc(128000L);
+                interimData.eventSize = fread(interimData.eventBuffer, 1, 128000L, fout);
+                interimData.eventBuffer = (char *) realloc(interimData.eventBuffer, interimData.eventSize);
+                interimData.tempEvents = (OLDNEWS *) interimData.eventBuffer;
 
-                while (left == 32000) {
-                    left = fread(scratch, 1, 32000, fout);
-                    fwrite(scratch, left, 1, fin);
-                }
-
-                fclose(fout); // close EVENT.TMP
                 fclose(fin);
             }  // end done if
 
@@ -855,15 +860,9 @@ void FileAccess(char mode)
                 interimData.replaySize = sizeof(REPLAY) * MAX_REPLAY_ITEMS;
                 fwrite(interimData.tempReplay, interimData.replaySize, 1, fin);
 
-                fout = sOpen("EVENT.TMP", "rb", 1); // Save Event File
-                left = 32000; // copy EVENT.TMP FILE
+                // Save Event Data
+                fwrite(interimData.eventBuffer, interimData.eventSize, 1, fin);
 
-                while (left == 32000) {
-                    left = fread(scratch, 1, 32000, fout);
-                    fwrite(scratch, left, 1, fin);
-                }
-
-                fclose(fout); // close EVENT.TMP
                 fclose(fin);
             }
 
@@ -1044,10 +1043,8 @@ void FileAccess(char mode)
 void
 save_game(char *name)
 {
-    FILE *inf, *outf;
+    FILE *outf;
     SaveFileHdr hdr;
-    size_t buflen = 0;
-    ssize_t size = 0;
     char *buf = NULL;
 
     EndOfTurnSave((char *) Data, sizeof(struct Players));
@@ -1088,30 +1085,13 @@ save_game(char *name)
     interimData.replaySize = sizeof(REPLAY) * MAX_REPLAY_ITEMS;
     fwrite(interimData.tempReplay, interimData.replaySize, 1, outf);
 
-    if ((inf = sOpen("EVENT.TMP", "rb", 1)) != NULL) {
-        size = fread_dyn(&buf, &buflen, inf);
-
-        if (size >= 0) {
-            fwrite(buf, size, 1, outf);
-        } else {
-            WARNING1("read error in EVENT.TMP");
-            goto cleanup;
-        }
-
-        fclose(inf);
-        inf = NULL;
-    } else {
-        WARNING1("can't read EVENT.TMP");
-    }
+    // Copy Event data into Save file
+    fwrite(interimData.eventBuffer, interimData.eventSize, 1, outf);
 
 cleanup:
 
     if (outf) {
         fclose(outf);
-    }
-
-    if (inf) {
-        fclose(inf);
     }
 
     if (buf) {

src/game/budget.cpp
@@ -694,21 +694,19 @@ void DrawVText(char got)
 
 int RetFile(char plr, int card)
 {
-    OLDNEWS oldNews;
+    OLDNEWS *oldNews;
     int bline, i;
-    FILE *fin;
-    memset(buffer, 0x00, BUFFER_SIZE);
-    fin = sOpen("EVENT.TMP", "rb", 1);
-    fseek(fin, (card + plr * 42) * sizeof(OLDNEWS), SEEK_SET);
-    fread(&oldNews, sizeof(OLDNEWS), 1, fin);
-    fseek(fin, oldNews.offset, SEEK_SET);
-    fread(buffer, oldNews.size, 1, fin);
-    fclose(fin);
+
+    oldNews = &interimData.tempEvents[card + plr * 42];
+    sprintf(buffer, "%s", interimData.eventBuffer + oldNews->offset);
+
     bline = 0;
 
-    for (i = 0; i < (int)strlen(buffer); i++) if (buffer[i] == 'x') {
+    for (i = 0; i < (int)strlen(buffer); i++) {
+        if (buffer[i] == 'x') {
             bline++;
         }
+    }
 
     bline -= 8;
 

src/game/news.cpp
@@ -566,11 +566,9 @@ News(char plr)
 {
     int bline = 0, ctop = 0, i;
     char cYr[5];
-    OLDNEWS oldNews;
     char loc = 0;
     uint8_t Status = 0, BW = 0;
     mm_file video_file, *fp = &video_file;
-    FILE *fout = NULL;
 
     BW = (Data->Year <= 63);
 
@@ -601,14 +599,12 @@ News(char plr)
 
     // File Structure is 84 longs 42 per side
 
-    fout = sOpen("EVENT.TMP", "r+b", 1);
-    fseek(fout, 0, SEEK_END);
-    oldNews.offset = ftell(fout);
-    oldNews.size = (strlen(buffer));
-    fwrite(buffer, strlen(buffer), 1, fout);
-    fseek(fout, (plr * 42 + Data->P[plr].eCount - 1) * sizeof(OLDNEWS), SEEK_SET);
-    fwrite(&oldNews, sizeof(OLDNEWS), 1, fout);
-    fclose(fout);
+    interimData.tempEvents[plr * 42 + Data->P[plr].eCount - 1].offset = interimData.eventSize;
+    interimData.tempEvents[plr * 42 + Data->P[plr].eCount - 1].size = strlen(buffer);
+    interimData.eventBuffer = (char *) realloc(interimData.eventBuffer, interimData.eventSize + strlen(buffer));
+    memcpy(interimData.eventBuffer + interimData.eventSize, buffer, strlen(buffer));
+    interimData.tempEvents = (OLDNEWS *) interimData.eventBuffer;
+    interimData.eventSize += strlen(buffer);
 
     /** \todo there is also M_NEW1950, why it is unused? */
     music_start(M_NEW1970);

src/game/start.cpp
@@ -50,7 +50,6 @@ void UpdateHardTurn(char plr);
 #define EVENT_PERIOD_POOL 5
 
 /* Initialize the event cards */
-FILE *fout;
 void InitializeEvents(void)
 {
     int cardCount = 2;  // Starting event card index
@@ -127,17 +126,11 @@ random_card:
     interimData.replaySize = sizeof(REPLAY) * MAX_REPLAY_ITEMS;
     memset(interimData.tempReplay, 0, interimData.replaySize);
 
-    OLDNEWS oldNews;
-    fout = sOpen("EVENT.TMP", "wb", 1);
-    memset(&oldNews, 0x00, sizeof oldNews);
-
-    for (j = 0; j < 84; j++) {
-        fwrite(&oldNews, sizeof(OLDNEWS), 1, fout);
-    }
-
-    fclose(fout);
-
-    return;
+    // Initialize in memory for Event Saves
+    interimData.eventSize = 84 * sizeof(OLDNEWS);
+    interimData.eventBuffer = (char *) malloc(interimData.eventSize);
+    memset(interimData.eventBuffer, 0, interimData.eventSize);
+    interimData.tempEvents = (OLDNEWS *) interimData.eventBuffer;
 }
 
 /**

commit e354a86cfec02781caa715abaf8d9ef84f4ef873

Fix error writing news events to save files
Corrects an error writing the normal save file introduced in commit
eb75c92c3ca0a775f6aed332d270ba31dc623393.

src/game/admin.cpp
@@ -812,11 +812,7 @@ void FileAccess(char mode)
                 fwrite(interimData.tempReplay, interimData.replaySize, 1, fin);
 
                 // Save Event information
-                // todo: how many bytes do we read?
-                interimData.eventBuffer = (char *) malloc(128000L);
-                interimData.eventSize = fread(interimData.eventBuffer, 1, 128000L, fout);
-                interimData.eventBuffer = (char *) realloc(interimData.eventBuffer, interimData.eventSize);
-                interimData.tempEvents = (OLDNEWS *) interimData.eventBuffer;
+                fwrite(interimData.eventBuffer, interimData.eventSize, 1, fin);
 
                 fclose(fin);
             }  // end done if