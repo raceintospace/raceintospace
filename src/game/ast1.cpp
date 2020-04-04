raceintospace/raceintospace

commit 8ea57684b42bd87e2d01caafcf4aeda9cb4c7efa

Add option to always enable female recruits
Creates a config option which allows for female astronauts/cosmonauts to
be recruited without requiring the corresponding news event.

src/game/ast1.cpp
@@ -502,9 +502,17 @@ void RandomizeNauts()
 
 void AstSel(char plr)
 {
-    char i, j, k, BarA, BarB, MaxMen, Index, now, now2, max, min, count, fem = 0, ksel = 0;
+    char i, j, k, BarA, BarB, MaxMen, Index, now, now2, max, min, count,
+         ksel = 0;
     FILE *fin;
+
+    bool femaleAstronautsAllowed =
+        (options.feat_female_nauts ||
+         Data->P[plr].FemaleAstronautsAllowed == 1);
+    bool femaleAstronautsRequired = Data->P[plr].FemaleAstronautsAllowed;
+
     MaxMen = Index = now = now2 = max = min = count = 0;
+
     music_start(M_DRUMSM);
     DrawAstCheck(plr);
     WaitForMouseUp();
@@ -585,41 +593,24 @@ void AstSel(char plr)
 
     switch (Data->P[plr].AstroLevel) {
     case 0:
-        MaxMen = 10;
+        MaxMen = femaleAstronautsAllowed ? 13 : 10;
         MaxSel = ASTRO_POOL_LVL1;
         Index = 0;
-
-        if (Data->P[plr].FemaleAstronautsAllowed == 1) {
-            MaxMen += 3;
-            fem = 1;
-        }
-
+        // TODO: This is an ugly hack...
         Data->P[plr].Cash -= 5;
         Data->P[plr].Spend[0][2] += 5;
         break;
 
     case 1:
-        MaxMen = 17;
+        MaxMen = femaleAstronautsAllowed ? 20 : 17;
         MaxSel = ASTRO_POOL_LVL2;
         Index = 14;
-
-        if (Data->P[plr].FemaleAstronautsAllowed == 1) {
-            MaxMen += 3;
-            fem = 1;
-        }
-
         break;
 
     case 2:
-        MaxMen = 19;
+        MaxMen = femaleAstronautsAllowed ? 22 : 19;
         MaxSel = ASTRO_POOL_LVL3;
         Index = 35;
-
-        if (Data->P[plr].FemaleAstronautsAllowed == 1) {
-            MaxMen += 3;
-            fem = 1;
-        }
-
         break;
 
     case 3:
@@ -1015,23 +1006,25 @@ void AstSel(char plr)
         }
 
         if ((x >= 245 && y >= 5 && x <= 314 && y <= 17 && mousebuttons > 0) || key == K_ENTER) { /* Exit - not 'til done */
-            if (fem == 1) {
-                j = 0;
+            bool femaleAstronautsSelected = false;
 
+            if (femaleAstronautsRequired) {
                 for (i = 0; i < count; i++) {
                     if (Men[sel[i]].Sex == 1) {
-                        j++;
+                        femaleAstronautsSelected = true;
+                        break;
                     }
                 }
 
-                if (j > 0) {
-                    fem = 0;
-                } else {
+                if (! femaleAstronautsSelected) {
+                    InBox(245, 5, 314, 17);
                     Help("i100");
+                    OutBox(245, 5, 314, 17);
                 }
             }
 
-            if (fem == 0 && count == MaxSel) {
+            if ((! femaleAstronautsRequired || femaleAstronautsSelected) &&
+                count == MaxSel) {
                 InBox(245, 5, 314, 17);
                 WaitForMouseUp();
 

src/game/aipur.cpp
@@ -449,7 +449,25 @@ void AIRandomizeNauts()
 }
 
 
-/** Select the best crew for the mission
+/**
+ * Select the best astronauts from the next class for recruitment.
+ *
+ * How the AI handles female astronauts/cosmonauts is a configurable
+ * feature, depending on options.feat_female_nauts. Options are
+ *   0: Classic mode (event-driven)
+ *   1: Always for players, Game decides AI behavior
+ * Greater specificity may be available, but is not guaranteed. Under
+ * the current setup
+ *   2: AI requires news event
+ *   3: AI acts as if under news event
+ *   4: AI is gender-blind [default]
+ * Leon has suggested default behavior be based on AI astronaut
+ * difficulty.
+ *
+ * Also noteworthy is that AI nauts don't suffer skill loss upon
+ * selection. This is to skip the difficulty of managing them in
+ * Basic training - from which they are automatically withdrawn -
+ * and therefore cannot benefit from.
  */
 void SelectBest(char plr, int pos)
 {
@@ -457,6 +475,16 @@ void SelectBest(char plr, int pos)
     FILE *fin;
     char tot, done;
     struct BuzzData *pData = &Data->P[plr];
+
+    // pData->FemaleAstronautsAllowed is the news event flag that
+    // allows & requires female astronauts.
+    // The configurable option should not affect its value.
+    bool femaleAstronautsAllowed =
+        pData->FemaleAstronautsAllowed == 1 ||
+        (options.feat_female_nauts > 0 && options.feat_female_nauts != 2);
+    bool femaleAstronautsRequired =
+        pData->FemaleAstronautsAllowed == 1 ||
+        options.feat_female_nauts == 3;
 
     for (i = 0; i < 25; i++) {
         AIsel[i] = 0;
@@ -475,36 +503,21 @@ void SelectBest(char plr, int pos)
 
     switch (pData->AstroLevel) {
     case 0:
-        MaxMen = 10;
+        MaxMen = femaleAstronautsAllowed ? 13 : 10;
         AIMaxSel = ASTRO_POOL_LVL1;
         Index = 0;
-
-        if (pData->FemaleAstronautsAllowed == 1) {
-            MaxMen += 3;
-        }
-
         break;
 
     case 1:
-        MaxMen = 17;
+        MaxMen = femaleAstronautsAllowed ? 20 : 17;
         AIMaxSel = ASTRO_POOL_LVL2;
         Index = 14;
-
-        if (pData->FemaleAstronautsAllowed == 1) {
-            MaxMen += 3;
-        }
-
         break;
 
     case 2:
-        MaxMen = 19;
+        MaxMen = femaleAstronautsAllowed ? 22 : 19;
         AIMaxSel = ASTRO_POOL_LVL3;
         Index = 35;
-
-        if (pData->FemaleAstronautsAllowed == 1) {
-            MaxMen += 3;
-        }
-
         break;
 
     case 3:
@@ -520,6 +533,7 @@ void SelectBest(char plr, int pos)
         break;
 
     default:
+        // TODO: Log an error...
         MaxMen = 0;
         AIMaxSel = 0;
         Index = 0;
@@ -527,9 +541,10 @@ void SelectBest(char plr, int pos)
     }
 
     now = Index;
-
     count = 0;
 
+    // TODO: This is a crude way of ordering all the candidates.
+    // It could be replaced with a superior method.
     for (i = 16; i > 0; i--) {
         done = 0;
 
@@ -539,7 +554,7 @@ void SelectBest(char plr, int pos)
 
                 if (i == tot) {
                     AIsel[count++] = j;
-                } else if (pData->FemaleAstronautsAllowed == 1 && Men[j].Sex == 1) {
+                } else if (femaleAstronautsRequired && Men[j].Sex == 1) {
                     AIsel[count++] = j;
                 }
             }

src/game/options.cpp
@@ -115,6 +115,12 @@ static struct {
         "Set to non-zero to shorten Advanced Training duration from 4 to 3 seasons."
     },
     {
+        "female_nauts", &options.feat_female_nauts, "%u", 0,
+        "Set to determine if female astronauts may be recruited:"
+        "\n#   0  Classic (only after a news event)"
+        "\n#   1  Always allow recruitment"
+    },
+    {
         "random_nauts", &options.feat_random_nauts, "%u", 0,
         "Set to non-zero to enable randomization of nauts."
     },  //Naut Randomize, Nikakd, 10/8/10
@@ -422,6 +428,7 @@ setup_options(int argc, char *argv[])
     options.want_fullscreen = 0;
     options.want_debug = 0;
     options.feat_shorter_advanced_training = 0;
+    options.feat_female_nauts = 0;
     options.feat_random_nauts = 0;   //Naut Randomize, Nikakd, 10/8/10
     options.feat_compat_nauts = 10;  //Naut Compatibility, Nikakd, 10/8/10
     options.feat_no_cTraining = 1;   //No Capsule Training, Nikakd, 10/8/10

src/game/options.h
@@ -10,6 +10,7 @@ typedef struct {
     unsigned want_cheats;
     unsigned want_debug;
     unsigned feat_shorter_advanced_training;
+    unsigned feat_female_nauts;
     unsigned feat_random_nauts;
     unsigned feat_compat_nauts;
     unsigned feat_no_cTraining;

commit 4ab848c845d10c4c9c8c7fdcb00e1457eab43631

Skip female 'naut requirement for groups IV and V
Corrects a bug where a female astronaut/cosmonaut requirement could
result in the player getting stuck. Commit
8ea57684b42bd87e2d01caafcf4aeda9cb4c7efa introduced a bug where the news
event requiring female 'nauts was not ignored for groups IV and V,
which contain no female candidates. Consequently, the player would be
locked into the recruitment screen, unable to proceed. This disables the
recruitment requirement for groups IV and V.

src/game/ast1.cpp
@@ -617,12 +617,14 @@ void AstSel(char plr)
         MaxMen = 27;
         MaxSel = ASTRO_POOL_LVL4;
         Index = 58;
+        femaleAstronautsRequired = false;
         break;
 
     case 4:
         MaxMen = 19;
         MaxSel = ASTRO_POOL_LVL5;
         Index = 86;
+        femaleAstronautsRequired = false;
         break;
     }
 