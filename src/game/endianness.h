1c9c63736c94f2d244f293b0a66809816ce5a374
Petr Menšík <pihhan@gmail.com>
2020-08-11 10:48:43 +0200

    Compile with boost 1.73
    Use more recent way to autodetect endianess. Try to keep backward
    compatibility for some time, use BOOST_VERSION macro to decide which
    header to use.

--- src/game/endianness.h
+++ src/game/endianness.h
@@ -1,7 +1,12 @@
 #ifndef __ENDIANNESS_H__
 #define __ENDIANNESS_H__
 
-#include <boost/detail/endian.hpp>
+#include <boost/version.hpp>
+#if BOOST_VERSION >= 106800
+#  include <boost/predef/other/endian.h>
+#else
+#  include <boost/detail/endian.hpp>
+#endif
 
 #include "data.h"
 
@@ -13,7 +18,7 @@ void _SwapEquipment(void);
 void _SwapPatchHdr(PatchHdr *hdr);
 void _SwapPatchHdrSmall(PatchHdrSmall *hdr);
 
-#ifdef BOOST_BIG_ENDIAN
+#if BOOST_ENDIAN_BIG_BYTE || defined(BOOST_BIG_ENDIAN)
 
 #define Swap32bit(a)  (a) = _Swap32bit((a))
 #define Swap16bit(a)  (a) = _Swap16bit((a))
@@ -23,8 +28,7 @@ void _SwapPatchHdrSmall(PatchHdrSmall *hdr);
 #define SwapPatchHdr(a) _SwapPatchHdr((a))
 #define SwapPatchHdrSmall(a) _SwapPatchHdrSmall((a))
 
-#else
-#ifdef BOOST_LITTLE_ENDIAN
+#elif BOOST_ENDIAN_LITTLE_BYTE || defined(BOOST_LITTLE_ENDIAN)
 
 #define Swap32bit(a)
 #define Swap16bit(a)
@@ -36,8 +40,5 @@ void _SwapPatchHdrSmall(PatchHdrSmall *hdr);
 #else
 #error "Uh oh! I don't know which endianness to pick."
 #endif
-
-#endif
-
 
 #endif // __ENDIANNESS_H__
