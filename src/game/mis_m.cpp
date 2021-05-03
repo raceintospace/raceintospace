raceintospace/raceintospace

commit 110c54b0c9a77098c28c2a417699dc6bbc1d051c

Kill the correct crew on LM mission failure steps
Fix an incorrect Lunar Module crew assignment when handling mission step
failures. Code 31 step failures kill the entire LM crew, previously
implemented as the LM pilot and EVA specialist. However, the lunar
landing code DrawMoonSelection() uses the LM pilot, plus the Command
pilot for 2-man LMs. Keeping to history, the LM will be staffed by the
Mission Commander (in-game the Command pilot) and LM pilot.

Using the Mission Commander and LM pilot removes a hack by which the
Capsule Pilot (aka Docking Specialist) was set as EVA specialist for
missions using the 2-man LM. This hack meant the wrong crew member would
perform spacewalks on LM Test EVA missions.

The Mission Commander dying on the LM introduces a new bug whereby their
CA skill is added to subsequent Capsule tests, but _that is entirely
consistent with current (bugged) mission death behavior_.

src/game/mis_m.cpp
@@ -26,6 +26,7 @@
 // This file is part of mission branching and failure handling
 
 #include <cassert>
+#include <vector>
 
 #include "display/graphics.h"
 #include "display/surface.h"
@@ -68,6 +69,7 @@ int MCGraph(char plr, int lc, int safety, int val, char prob);
 void F_KillCrew(char mode, struct Astros *Victim);
 void F_IRCrew(char mode, struct Astros *Guy);
 int FailEval(char plr, int type, char *text, int val, int xtra);
+std::vector<Astros *> LMCrew(int pad, Equipment *module);
 void InvalidatePrestige();
 
 /**
@@ -767,6 +769,20 @@ int MCGraph(char plr, int lc, int safety, int val, char prob)
 #define F_ALL 0
 #define F_ONE 1
 
+/**
+ * Kill one or all member(s) of the flight crew.
+ *
+ * TODO: The safety penalty suffered by the hardware program is applied
+ * each time this method is called. While it is supposed to be a lesser
+ * penalty on the lowest difficulty, that penalty may be triggered
+ * multiple times if crew members are all killed with individual calls
+ * (ex: Lunar Module).
+ *
+ * TODO: Reassign mission roles for any remaining crew.
+ *
+ * \param mode  F_ONE or F_ALL
+ * \param Victim  if F_ONE is set, the crew member to kill.
+ */
 void F_KillCrew(char mode, struct Astros *Victim)
 {
     int k = 0, p = 0;
@@ -779,6 +795,7 @@ void F_KillCrew(char mode, struct Astros *Victim)
         p = 1;
     }
 
+    // TODO: Need to set the Base as the minimum value when dividing by 2
     if ((Data->Def.Lev1 == 0 && p == 0) || (Data->Def.Lev2 == 0 && p == 1)) {
         Mev[STEP].E->Safety /= 2;
     } else {
@@ -1223,27 +1240,16 @@ int FailEval(char plr, int type, char *text, int val, int xtra)
 
         break;
 
-    case 31:  // kill EVA and LM people and branch VAL steps
-        //Dock_Skip=1;
+    case 31:  // kill LM crew and branch VAL steps
         Mev[STEP].trace = Mev[STEP].dgoto;
         Mev[STEP].StepInfo = 3100 + STEP;
 
-        if (strncmp(Mev[STEP].E->ID, "C4", 2) == 0) {
-            F_KillCrew(F_ALL, 0);
-            Mev[STEP].StepInfo = 4100 + STEP;
-        } else {
-            if (JOINT == 0) {
-                F_KillCrew(F_ONE, MA[0][EVA[0]].A);
+        {
+            std::vector<Astros *> crew = LMCrew(Mev[STEP].pad, Mev[STEP].E);
 
-                if (LM[0] != EVA[0]) {
-                    F_KillCrew(F_ONE, MA[0][LM[0]].A);
-                }
-            } else {
-                F_KillCrew(F_ONE, MA[1][EVA[1]].A);
-
-                if (LM[1] != EVA[1]) {
-                    F_KillCrew(F_ONE, MA[1][LM[1]].A);
-                }
+            for (std::vector<Astros *>::iterator it = crew.begin();
+                 it != crew.end(); it++) {
+                F_KillCrew(F_ONE, *it);
             }
         }
 
@@ -1360,6 +1366,41 @@ int FailEval(char plr, int type, char *text, int val, int xtra)
     death = 0;
     return FNote;
 }
+
+
+/**
+ * Return the crew for the Lunar Module.
+ *
+ * The crew of a Lunar Module consists of the LM specialist for a
+ * one-person module, adding the mission commander for a two-person LM.
+ *
+ * \param pad  0 for primary launch, 1 for secondary launch
+ * \param module  The lunar module
+ */
+std::vector<Astros *> LMCrew(int pad, Equipment *module)
+{
+    assert(pad >= 0 && pad <= JOINT);
+    assert(module && HardwareType(*module) == Mission_LM);
+
+    std::vector<Astros *> crew;
+    int capacity = CrewSize(*module);
+
+    if (LM[pad] > 0 && MA[pad][LM[pad]].A) {
+        crew.push_back(MA[pad][LM[pad]].A);
+    }
+
+    if (capacity == 2 && CAP[pad] >= 0 && MA[pad][CAP[pad]].A) {
+        crew.push_back(MA[pad][CAP[pad]].A);
+    }
+
+    if (crew.size() != capacity) {
+        ERROR3("LM module should have %d crew, only found %d",
+               capacity, crew.size());
+    }
+
+    return crew;
+}
+
 
 /* Set the PComp flag such that prestige is not being awarded for step
  * failures.

src/game/hardware.cpp
@@ -15,13 +15,106 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
+#include "hardware.h"
+
 #include <cassert>
 #include <stdexcept>
 
-#include "Buzz_inc.h"
-#include "data.h"
 #include "game_main.h"
+#include "macros.h"
 #include "options.h"
+
+
+/**
+ * Check the crew capacity for a Capsule/Minishuttle or Lunar Module.
+ *
+ * \param program  a Manned hardware program.
+ * \return  number of crew allowed/required (0 if unmanned).
+ */
+int CrewSize(const Equipment &program)
+{
+    MissionHardwareType type = HardwareType(program);
+
+    if (type != Mission_Capsule && type != Mission_LM) {
+        return 0;
+    }
+
+    int mannedIndex = int(program.ID[1] - '0');
+
+    switch (mannedIndex) {
+    case MANNED_HW_ONE_MAN_CAPSULE:
+        return 1;
+
+    case MANNED_HW_TWO_MAN_CAPSULE:
+        return 2;
+
+    case MANNED_HW_THREE_MAN_CAPSULE:
+    case MANNED_HW_MINISHUTTLE:
+        return 3;
+
+    case MANNED_HW_FOUR_MAN_CAPSULE:
+        return 4;
+
+    case MANNED_HW_TWO_MAN_MODULE:
+        return 2;
+
+    case MANNED_HW_ONE_MAN_MODULE:
+        return 1;
+
+    default:
+        return 0;
+    }
+
+    return 0;
+}
+
+
+/**
+ * TODO: Describe.
+ *
+ * If the argument is not a recognized hardware program, the
+ * result is undefined.
+ */
+MissionHardwareType HardwareType(const Equipment &program)
+{
+    switch (program.ID[0]) {
+    case 'P':
+        return Mission_Probe_DM;
+
+    case 'C':
+        return (program.ID[1] <= MANNED_HW_FOUR_MAN_CAPSULE)
+               ? Mission_Capsule : Mission_LM;
+
+    case 'R':
+        return (program.ID[1] != ROCKET_HW_BOOSTERS)
+               ? Mission_PrimaryBooster : Mission_SecondaryBooster;
+
+    case 'M':
+        if (program.ID[1] <= MISC_HW_KICKER_C) {
+            return Mission_Kicker;
+        } else if (program.ID[1] == MISC_HW_EVA_SUITS) {
+            return Mission_EVA;
+        } else if (program.ID[1] == MISC_HW_DOCKING_MODULE) {
+            return Mission_Probe_DM;
+        } else if (program.ID[1] == MISC_HW_PHOTO_RECON) {
+            return Mission_PhotoRecon;
+        } else {
+            throw std::invalid_argument(
+                "Error in function HardwareType: "
+                "argument program is not recognized Misc hardware");
+        }
+
+    default:
+        throw std::invalid_argument(
+            "Error in function HardwareType: "
+            "program's hardware index is not recognized.");
+        break;
+    }
+
+    throw std::invalid_argument(
+        "Error in function HardwareType: "
+        "program's hardware index is not recognized.");
+}
 
 
 /**

src/game/hardware.h
@@ -1,8 +1,11 @@
 #ifndef HARDWARE_H
 #define HARDWARE_H
 
+#include "data.h"
 
+int CrewSize(const Equipment &program);
 Equipment &HardwareProgram(int player, int type, int program);
+MissionHardwareType HardwareType(const Equipment &program);
 int RocketBoosterSafety(int safetyRocket, int safetyBooster);
 
 

src/game/mc.cpp
@@ -221,18 +221,9 @@ int Launch(char plr, char mis)
             break;  // Last in LM
 
         case 3:
-
-            // LM is always on first mission part!!! :: makes things easier
-            if (MH[0][Mission_LM] && MH[0][Mission_LM]->ID[1] == 0x35) {
-                CAP[i] = 0;
-                LM[i] = 1;
-                DOC[i] = EVA[i] = 2;
-            } else {
-                CAP[i] = 0;
-                LM[i] = EVA[i] = 1;
-                DOC[i] = 2;
-            }
-
+            CAP[i] = 0;
+            LM[i] = EVA[i] = 1;
+            DOC[i] = 2;
             break;
 
         case 4:

commit 470c7aeab2f0a13cc046e1f4e49c734c9eb1da5c

Fix game crash when photo recon kills LM crew
Fixes a bug where a failed Photo Recon check resulting in LM crew death
causes the game to crash. Commit 110c54b0c9a mistakenly assumed that LM
crew deaths would always be caused by LM failures. This meant Photo
Recon failures were not correctly specifying the Lunar Module used; this
tripped an assertion.

src/game/mis_m.cpp
@@ -69,6 +69,7 @@ int MCGraph(char plr, int lc, int safety, int val, char prob);
 void F_KillCrew(char mode, struct Astros *Victim);
 void F_IRCrew(char mode, struct Astros *Guy);
 int FailEval(char plr, int type, char *text, int val, int xtra);
+Equipment* FindLunarModule();
 std::vector<Astros *> LMCrew(int pad, Equipment *module);
 void InvalidatePrestige();
 void BranchIfAlive(int *FNote);
@@ -1222,8 +1223,11 @@ int FailEval(char plr, int type, char *text, int val, int xtra)
         Mev[STEP].trace = Mev[STEP].dgoto;
         Mev[STEP].StepInfo = 3100 + STEP;
 
+        // This error can occur on Photo Recon tests, so the LM must
+        // be identified first.
         {
-            std::vector<Astros *> crew = LMCrew(Mev[STEP].pad, GetEquipment(Mev[STEP]));
+            std::vector<Astros *> crew =
+                LMCrew(Mev[STEP].pad, FindLunarModule());
 
             for (std::vector<Astros *>::iterator it = crew.begin();
                  it != crew.end(); it++) {
@@ -1384,6 +1388,23 @@ int FailEval(char plr, int type, char *text, int val, int xtra)
 
     death = 0;
     return FNote;
+}
+
+
+/**
+ * Return the active Lunar Module for the current mission.
+ *
+ * This is used to find the LM for steps that affect the LM but do
+ * not test the LM directly, such as Photo Recon tests.
+ *
+ * \return the LM, or NULL if there is none for the mission.
+ */
+Equipment* FindLunarModule()
+{
+    // For joint missions, the LM is always found on the first launch,
+    // because if there's a problem launching it, there's no reason to
+    // risk the manned launch.
+    return MH[0][Mission_LM];
 }
 
 