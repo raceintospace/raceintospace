c1c6b2581994b6db812f38c5676d8b3d8e4f00c6
NooneAtAll3-2 <NooneAtAll3+2@proton.me>
2025-12-03 21:14:20 +0000

    Added Multimedia class to mmfile.h

--- src/game/mmfile.h
+++ src/game/mmfile.h
@@ -2,12 +2,148 @@
 #define _MM_FILE_H
 
 #include <cstdio>
+#include <memory>
 
 #include <ogg/ogg.h>
 #include <vorbis/codec.h>
 #include <theora/theora.h>
 #include <SDL.h>
 
+enum media_type : bool
+{
+    AUDIO = false,
+    VIDEO = true
+};
+
+struct Theora_info_raii
+{
+    std::unique_ptr<theora_info> th_info;
+
+    Theora_info_raii()
+    : th_info{new theora_info{}}
+    {
+        theora_info_init(th_info.get());
+    }
+
+    theora_info* get() {return th_info.get();}
+    theora_info* release() {return th_info.release();}
+
+    ~Theora_info_raii()
+    {
+        if (th_info == nullptr) return;        
+        theora_info_clear(th_info.get());
+    }
+};
+
+struct Theora_comment_raii
+{
+    theora_comment th_comm;
+
+    Theora_comment_raii()
+    {
+        theora_comment_init(&th_comm);
+    }
+
+    theora_comment* get()
+    {
+        return &th_comm;
+    }
+
+    ~Theora_comment_raii()
+    {
+        theora_comment_clear(&th_comm);
+    }
+};
+
+struct Ogg_stream_raii
+{
+    std::unique_ptr<ogg_stream_state> stream;
+
+    Ogg_stream_raii() = default;
+
+    Ogg_stream_raii(ogg_page* pg)
+    : stream{new ogg_stream_state{}}
+    {
+        ogg_stream_init(stream.get(), ogg_page_serialno(pg));
+    }
+
+    ogg_stream_state* get() {return stream.get();}
+    ogg_stream_state* release() {return stream.release();}
+
+    ~Ogg_stream_raii()
+    {
+        if (stream == nullptr) return;
+        ogg_stream_clear(stream.get());
+    }
+};
+
+struct Vorbis_info_raii
+{
+    std::unique_ptr<vorbis_info> vo_info;
+
+    Vorbis_info_raii()
+    : vo_info{new vorbis_info{}}
+    {
+        vorbis_info_init(vo_info.get());
+    }
+
+    vorbis_info* get() {return vo_info.get();}
+    vorbis_info* release() {return vo_info.release();}
+
+    ~Vorbis_info_raii()
+    {
+        if (vo_info == nullptr) return;
+        vorbis_info_clear(vo_info.get());
+    }
+};
+
+class Multimedia
+{
+public:
+    bool ignore_audio{};
+    bool ignore_video{};
+
+private:
+    bool good{true};
+    bool stream_has_ended[2]{};
+    struct {
+        FILE *file;
+        ogg_sync_state sync;
+
+        Ogg_stream_raii audio;
+        Vorbis_info_raii audio_info;
+        std::unique_ptr<vorbis_dsp_state> audio_ctx;
+        std::unique_ptr<vorbis_block> audio_blk;
+
+        Ogg_stream_raii video;
+        Theora_info_raii video_info;
+        std::unique_ptr<theora_state> video_ctx;
+    } mmf{};
+
+public:
+    Multimedia(FILE* fp);
+    Multimedia& operator=(Multimedia&&);
+    ~Multimedia();
+
+    bool is_good();
+
+    bool is_audio();
+    int channels();
+    int audio_rate();
+    int decode_audio(void* buf, int buflen); // returns number of bytes written to buffer
+
+    bool is_video();
+    float fps();
+    int w();
+    int h();
+    void draw_video_frame(SDL_Overlay& ovl);
+
+private:
+    ogg_page get_page();
+    void init_theora();
+    void init_vorbis();
+    ogg_packet get_packet(enum media_type is_video);
+};
 
 enum stream_type {
     MEDIA_AUDIO = 1,
