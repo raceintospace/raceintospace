b3c067dd2a2a12ef5bc857c3f0856f25b64f9ec5
NooneAtAll3-2 <NooneAtAll3+2@proton.me>
2026-01-16 09:01:53 +0000

    Remove mm_file and its functions
    Final steps of mm_file into Multimedia class refactor

--- src/game/mmfile.h
+++ src/game/mmfile.h
@@ -134,36 +134,4 @@ private:
     ogg_packet get_packet(enum media_type is_video);
 };
 
-enum stream_type {
-    MEDIA_AUDIO = 1,
-
-    MEDIA_VIDEO = 2
-};
-
-typedef struct {
-    FILE *file;
-    ogg_sync_state sync;
-    ogg_stream_state *audio;
-    vorbis_info *audio_info;
-    vorbis_dsp_state *audio_ctx;
-    vorbis_block *audio_blk;
-    ogg_stream_state *video;
-    theora_info *video_info;
-    theora_state *video_ctx;
-    unsigned end_of_stream;
-    unsigned drop_packets;
-} mm_file;
-
-extern int mm_open(mm_file *mf, const char *fname);
-extern int mm_open_fp(mm_file *mf, FILE *file);
-extern unsigned mm_ignore(mm_file *mf, unsigned mask);
-extern int mm_close(mm_file *mf);
-extern int mm_video_info(const mm_file *mf, unsigned *width, unsigned *height, float *fps);
-extern int mm_audio_info(const mm_file *mf, unsigned *channels, unsigned *rate);
-extern int mm_decode_video(mm_file *mf, SDL_Overlay *ovl);
-extern int mm_decode_audio(mm_file *mf, void *buf, int buflen);
-#if 0
-extern int mm_convert_audio(mm_file *mf, void *buf, int buflen, SDL_AudioSpec *spec);
-#endif
-
 #endif /* _MM_FILE_H */
