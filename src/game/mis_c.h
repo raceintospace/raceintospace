4fdec9ae45cac3b09cdd3be54272cb7dc28ec409
NooneAtAll3-2 <NooneAtAll3+2@proton.me>
2026-02-01 11:55:17 +0000

    Change parameter type of FailureMode() in mis_c.h

--- src/game/mis_c.h
+++ src/game/mis_c.h
@@ -4,14 +4,14 @@
 #include "data.h"
 
 void FirstManOnMoon(char plr, char isAI, char misNum,
-                    const struct MisEval &step);
-void PlaySequence(char plr, int step, const char *Seq, char mode);
-char FailureMode(char plr, int prelim, char *text);
+                    const MisEval& step);
+void PlaySequence(char plr, int step, const char* Seq, char mode);
+char FailureMode(char plr, int prelim, const char* text);
 void Tick(char plr);
 
 extern char daysAMonth[12];
 extern char STEPnum;
-extern struct AnimType AHead;
-extern struct BlockHead BHead;
+extern AnimType AHead;
+extern BlockHead BHead;
 
 #endif // MIS_C_H
