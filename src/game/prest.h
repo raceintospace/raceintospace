bf038a379d53661b776f038ae55cc716f7397adb
NooneAtAll3-2 <NooneAtAll3+2@proton.me>
2026-05-09 23:55:58 +0000

    Cleanup declarations in prest.h

--- src/game/prest.h
+++ src/game/prest.h
@@ -3,17 +3,17 @@
 
 #include "data.h"
 
-int AchievementPenalty(char plr, const struct mStr &mission);
-int DurationPenalty(char plr, const struct mStr &mission);
-int MilestonePenalty(char plr, const struct mStr &mission);
-int NewMissionPenalty(char plr, const struct mStr &mission);
+int AchievementPenalty(char plr, const mStr& mission);
+int DurationPenalty(char plr, const mStr& mission);
+int MilestonePenalty(char plr, const mStr& mission);
+int NewMissionPenalty(char plr, const mStr& mission);
 
 int PrestNeg(char plr, int i);
 char Set_Goal(char plr, char which, char control);
-int Find_MaxGoal(void);
+int Find_MaxGoal();
 int U_AllotPrest(char plr, char mis);
 int AllotPrest(char plr, char mis);
-int MaxFail(void);
+int MaxFail();
 int Update_Prestige_Data(char plr, char mis, int code);
 
 #endif // PREST_H
