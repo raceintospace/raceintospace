b07de5a8b8d1089f622d43d9c9a05d842723e3a0
NooneAtAll3-2 <NooneAtAll3+2@proton.me>
2026-05-10 00:31:43 +0000

    Cleanup declarations in astros.h

--- src/game/astros.h
+++ src/game/astros.h
@@ -5,7 +5,6 @@
 
 int AvailableCrewsCount(int plr, int program);
 void CheckFlightCrews(int player);
-bool Compatible(const struct Astros &ast, const struct Astros &peer);
-
+bool Compatible(const Astros& ast, const Astros& peer);
 
 #endif // ASTROS_H
