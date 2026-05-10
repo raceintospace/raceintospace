bad682bc52fb42778f292044f1280da11286e0c0
NooneAtAll3-2 <NooneAtAll3+2@proton.me>
2026-05-10 00:32:29 +0000

    Cleanup declarations in ast0.h

--- src/game/ast0.h
+++ src/game/ast0.h
@@ -3,8 +3,8 @@
 
 #include "data.h"
 
-void BarSkill(char plr, int lc, int nw, int *ary);
-void DispLeft(char plr, int lc, int cnt, int nw, const int *ary);
+void BarSkill(char plr, int lc, int nw, int* ary);
+void DispLeft(char plr, int lc, int cnt, int nw, const int* ary);
 void LMBld(char plr);
 int MoodColor(uint8_t mood);
 void Moon(char plr);
