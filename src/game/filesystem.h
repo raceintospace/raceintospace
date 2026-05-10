80b1afbdb989a5fb9957e6751a1470ddbb1dd7f6
NooneAtAll3-2 <NooneAtAll3+2@proton.me>
2026-05-10 00:19:07 +0000

    Cleanup declarations in filesystem.h

--- src/game/filesystem.h
+++ src/game/filesystem.h
@@ -18,18 +18,18 @@ protected:
     static Filesystem singleton;
 
 public:
-    static void init(const char *argv0);
-    static bool exists(const std::string &filename);
-    static std::list<std::string> enumerate(const std::string &directory);
-    static bool unlink(const std::string &filename);
+    static void init(const char* argv0);
+    static bool exists(const std::string& filename);
+    static std::list<std::string> enumerate(const std::string& directory);
+    static bool unlink(const std::string& filename);
 
-    static boost::shared_ptr<File> open(const std::string &filename);
+    static boost::shared_ptr<File> open(const std::string& filename);
 
-    static boost::shared_ptr<File> openWrite(const std::string &filename);
+    static boost::shared_ptr<File> openWrite(const std::string& filename);
 
-    static void readToBuffer(const std::string &filename, void *buffer, uint32_t length, uint32_t offset = 0);
-    static boost::shared_ptr<display::PalettizedSurface> readImage(const std::string &filename);
-    static void addPath(const char *s);
+    static void readToBuffer(const std::string& filename, void* buffer, uint32_t length, uint32_t offset = 0);
+    static boost::shared_ptr<display::PalettizedSurface> readImage(const std::string& filename);
+    static void addPath(const char* s);
 };
 
 #endif // FILESYSTEM_H
