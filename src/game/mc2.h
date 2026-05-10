522ed5a511e2202c1d275da8fffd1fc82b3fc8c6
NooneAtAll3-2 <NooneAtAll3+2@proton.me>
2026-05-10 00:03:45 +0000

    Cleanup declarations in mc2.h

--- src/game/mc2.h
+++ src/game/mc2.h
@@ -6,7 +6,7 @@
 void MissionCodes(char plr, char val, char pad);
 void MissionSetup(char plr, char mis);
 void MissionSetDown(char plr, char mis);
-void MisSkip(char plr, const struct mStr &mission);
+void MisSkip(char plr, const mStr& mission);
 void MisRush(char plr, char rush_level);
 
 #endif // MC2_H
