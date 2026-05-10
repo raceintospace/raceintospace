9bcaf0a861eea24fdba56c5f5d7547e98fa0aa88
NooneAtAll3-2 <NooneAtAll3+2@proton.me>
2026-05-10 00:08:31 +0000

    Cleanup declarations in hardware.h

--- src/game/hardware.h
+++ src/game/hardware.h
@@ -3,11 +3,10 @@
 
 #include "data.h"
 
-int CrewSize(const Equipment &program);
+int CrewSize(const Equipment& program);
 Equipment &HardwareProgram(int player, int type, int program);
-MissionHardwareType HardwareType(const Equipment &program);
+MissionHardwareType HardwareType(const Equipment& program);
 int RocketBoosterSafety(int safetyRocket, int safetyBooster);
 int RocketMaxRD(int MaxRDRocket, int MaxRDBooster);
-
 
 #endif // HARDWARE_H
