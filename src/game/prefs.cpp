67d80d5e34ce6b5cb7fb8ed4abef147365404120
Benoit Bregeault <b.bregeault@posteo.net>
2026-09-01 01:37:46 +0200

    Remove unreachable code and simplify 'where' comparisons in DrawPrefs() and Preferences()
    Preferences is always invoked from this file with parameter 'where' set to 0, 1 or 3. Remove 'where == 2' cases (was for play by modem) and simplify comparisons

--- src/game/prefs.cpp
+++ src/game/prefs.cpp
@@ -102,7 +102,7 @@ void DrawPrefs(int where, char a1, char a2, AudioConfig audio,
     fill_rectangle(96, 114, 223, 194, 0);
     ShBox(230, 24, 319, 199);
 
-    if (where == 0 || where == 3) {
+    if (where != 1) {
         music_start(M_SOVTYP);
         IOBox(6, 105, 83, 140);
         IOBox(6, 158, 83, 193);
@@ -134,12 +134,10 @@ void DrawPrefs(int where, char a1, char a2, AudioConfig audio,
         fill_rectangle(7, 35, 82, 41, 0);
     }
 
-    if (where != 2 && where != 3) {
-        draw_heading(6, 5, "PREFERENCES SELECTIONS", 0, -1);
-    } else if (where == 3) {
+    if (where == 3) {
         draw_heading(3, 5, "PLAY BY MAIL SELECTIONS", 0, -1);
     } else {
-        draw_heading(6, 5, "MODEM GAME SELECTIONS", 0, -1);
+        draw_heading(6, 5, "PREFERENCES SELECTIONS", 0, -1);
     }
 
     IOBox(243, 3, 316, 19);
@@ -154,7 +152,7 @@ void DrawPrefs(int where, char a1, char a2, AudioConfig audio,
     Levels(1, Data->Def.Lev2, 1, dctx);
     Levels(1, Data->Def.Ast2, 0, dctx);
 
-    if (where == 0 || where == 3) {
+    if (where != 1) {
         display::graphics.setForegroundColor(9);
     } else {
         display::graphics.setForegroundColor(34);
@@ -188,7 +186,6 @@ void DrawPrefs(int where, char a1, char a2, AudioConfig audio,
     display::graphics.legacyScreen()->draw(dctx.prefs_image, 72 * (Data->Def.Anim), 90, 71, 29, 147, 71);
     HModel(Data->Def.Input, 1);
 
-    // if (where==0 || where==2)
     FadeIn(2, 10, 0, 0);
 }
 
@@ -514,13 +511,13 @@ int Preferences(int player, int where)
                     Data->Def.Plr1 += hum1 * 2;
                     Data->Def.Plr2 += hum2 * 2;
 
-                    if (where == 0 || where == 3) {
+                    if (where != 1) {
                         FadeOut(2, 10, 0, 0);
                     }
 
                     key = 0;
 
-                    if ((where == 0 || where == 3) && (Data->Def.Input == 2 || Data->Def.Input == 3)) {
+                    if (where != 1 && (Data->Def.Input == 2 || Data->Def.Input == 3)) {
                         std::ifstream os{locate_file("hist.json", FT_DATA)};
                         cereal::JSONInputArchive ar{os};
 
@@ -539,7 +536,7 @@ int Preferences(int player, int where)
                     }
 
                     // Random Equipment
-                    if ((where == 0 || where == 3) && (Data->Def.Input == 4 || Data->Def.Input == 5)) {
+                    if (where != 1 && (Data->Def.Input == 4 || Data->Def.Input == 5)) {
                         RandomizeEq();
                     }
 
@@ -564,7 +561,7 @@ int Preferences(int player, int where)
                 music_stop();
                 FadeOut(2, 10, 0, 0);
                 return PREFS_ABORTED;
-            } else if (key == 'P' && (where == 0 || where == 3)) {
+            } else if (key == 'P' && where != 1) {
                 fill_rectangle(59, 26, 68, 31, 3);
                 fill_rectangle(290, 26, 298, 31, 3);
 
@@ -592,7 +589,7 @@ int Preferences(int player, int where)
                 DrawPrefs(where, hum1, hum2, audio, dctx);
 
             } else if (((x >= 96 && y >= 114 && x <= 223 && y <= 194 && mousebuttons > 0) || key == K_SPACE) 
-                       && (where == 3 || where == 0)) {  // Hist
+                       && where != 1) {  // Hist
                 char maxHModels;
                 maxHModels = options.feat_random_eq > 0 ? 5 : 3;
                 WaitForMouseUp();
@@ -654,8 +651,8 @@ int Preferences(int player, int where)
                 }
 
                 Levels(0, Data->Def.Lev1, 1, dctx);
-            } else if ((x >= 8 && y >= 107 && x <= 81 && y <= 138 && (where == 0 || where == 3) && mousebuttons > 0) 
-                       || ((where == 3 || where == 0) && ksel == 0 && key == 'G')) {
+            } else if ((x >= 8 && y >= 107 && x <= 81 && y <= 138 && where != 1 && mousebuttons > 0)
+                       || (where != 1 && ksel == 0 && key == 'G')) {
                 InBox(8, 107, 81, 138);
                 WaitForMouseUp();
                 OutBox(8, 107, 81, 138);
@@ -667,8 +664,8 @@ int Preferences(int player, int where)
 
                 Levels(0, Data->Def.Lev1, 1, dctx);
                 /* P1: Game Level */
-            } else if ((x >= 8 && y >= 160 && x <= 81 && y <= 191 && ((where == 0 || where == 3) && mousebuttons > 0)) 
-                       || ((where == 3 || where == 0) && ksel == 0 && key == 'L')) {
+            } else if ((x >= 8 && y >= 160 && x <= 81 && y <= 191 && (where != 1 && mousebuttons > 0))
+                       || (where != 1 && ksel == 0 && key == 'L')) {
                 InBox(8, 160, 81, 191);
                 WaitForMouseUp();
                 OutBox(8, 160, 81, 191);
@@ -702,8 +699,8 @@ int Preferences(int player, int where)
                 }
 
                 Levels(1, Data->Def.Lev2, 1, dctx);
-            } else if ((x >= 238 && y >= 107 && x <= 311 && y <= 138 && (where == 0 || where == 3) && mousebuttons > 0) 
-                       || ((where == 0 || where == 3) && ksel == 1 && key == 'G')) {
+            } else if ((x >= 238 && y >= 107 && x <= 311 && y <= 138 && where != 1 && mousebuttons > 0)
+                       || (where != 1 && ksel == 1 && key == 'G')) {
                 InBox(238, 107, 311, 138);
                 WaitForMouseUp();
                 OutBox(238, 107, 311, 138);
@@ -715,8 +712,8 @@ int Preferences(int player, int where)
 
                 Levels(1, Data->Def.Lev2, 1, dctx);
                 /* P2: Game Level */
-            } else if ((x >= 238 && y >= 160 && x <= 311 && y <= 191 && (where == 0 || where == 3) && mousebuttons > 0) 
-                       || ((where == 0 || where == 3) && ksel == 1 && key == 'L')) {
+            } else if ((x >= 238 && y >= 160 && x <= 311 && y <= 191 && where != 1 && mousebuttons > 0)
+                       || (where != 1 && ksel == 1 && key == 'L')) {
                 InBox(238, 160, 311, 191);
                 WaitForMouseUp();
                 OutBox(238, 160, 311, 191);
@@ -731,14 +728,14 @@ int Preferences(int player, int where)
             } else if ((x >= 6 && y >= 34 && x <= 83 && y <= 42 && mousebuttons > 0)
                        || (ksel == 0 && key == 'N')) {
                 /* P1: Director Name */
-                if (where == 0 || where == 3 ||
+                if (where != 1 ||
                     (where == 1 && (player == 0 || !IsHumanPlayer(0)))) {
                     EditDirectorName(0);
                 }
             } else if ((x >= 236 && y >= 34 && x <= 313 && y <= 42 && mousebuttons > 0)
                        || (ksel == 1 && key == 'N')) {
                 /* P2: Director Name */
-                if (where == 0 || where == 3 ||
+                if (where != 1 ||
                     (where == 1 && (player == 1 || !IsHumanPlayer(1)))) {
                     EditDirectorName(1);
                 }
